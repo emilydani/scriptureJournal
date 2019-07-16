43495eb80c0e2154f59b7fb1098cd19241c43ed6
emilydani <38664438+emilydani@users.noreply.github.com>
2019-07-16 12:36:53 -0700

    Add files via upload

---
+++ Models/Journal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScriptureJournal.Models
+{
+    public class Journal
+    {
+        public int ID { get; set; }
+
+
+
+        [StringLength(25, MinimumLength = 3)]
+        [Required]
+        public string Book { get; set; }
+        [Range(1, 150)]
+        [Required]
+        public int Chapter { get; set; }
+        [Range(1, 150)]
+        [Required]
+        public int Verse { get; set; }
+        [StringLength(254)]
+        [Required]
+        public string Note { get; set; }
+
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+
+
+      
+    }
+}
---
+++ Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ScriptureJournal.Models;
+
+namespace ScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ScriptureJournal.Models.ScriptureJournalContext _context;
+        public IndexModel(ScriptureJournal.Models.ScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        
+        public PaginatedList<Journal> Journal { get; set; }
+        public string SearchString { get; set; }
+        public string KeySearchString { get; set; }
+        public SelectList Book { get; set; }
+        public string Books { get; set; }
+        public string CurrentSort { get;  set; }
+
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+
+        public async Task OnGetAsync(string Books, string keySearchString, string searchString, string sortOrder, int? pageIndex)
+
+
+        {
+            CurrentSort = sortOrder;
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = SearchString;
+            }
+            
+            IQueryable<Journal> bookQuery = from s in _context.Book
+                                            select s;
+            
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                bookQuery = bookQuery.Where(s => s.Book.Contains(searchString));
+            }
+            if (!String.IsNullOrEmpty(keySearchString))
+            {
+                bookQuery = bookQuery.Where(x => x.Note.Contains(keySearchString));
+            }
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    bookQuery = bookQuery.OrderByDescending(s => s.Book);
+                    break;
+                case "Date":
+                    bookQuery = bookQuery.OrderBy(s => s.DateAdded);
+                    break;
+
+                default:
+                    bookQuery = bookQuery.OrderBy(s => s.Book);
+                    break;
+            }
+
+                        
+            SearchString = searchString;
+            int pageSize = 5;
+            Journal = await PaginatedList<Journal>.CreateAsync(bookQuery.AsNoTracking(), pageIndex ?? 1, pageSize);
+
+        }
+        
+    }
+}
